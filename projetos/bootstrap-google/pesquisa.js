bab3edec83cf4d2ca19a8989be3e9e9965554945
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-04-10 08:30:12 -0300

    correção da barra de pesquisa

--- projetos/bootstrap-google/pesquisa.js
+++ projetos/bootstrap-google/pesquisa.js
@@ -2,7 +2,7 @@ function pesquisar() {
     var pesquisou = document.getElementById('caixadepesquisa').value
     var wesley = 'wesley'
     if (pesquisou.toUpperCase() === wesley.toUpperCase()) {
-        window.open("wesley.html", "_self")
+        window.open("wesley.html", "_blank")
     } else {
         window.open("pagina-nav.html", "_blank")
     }
@@ -11,7 +11,7 @@ function pesquisar() {
 function pesquisar_barra(botao) {
     if (botao.key === 'enter') {
         if (pesquisou.toUpperCase() === wesley.toUpperCase()) {
-            window.open("wesley.html", "_self")
+            window.open("wesley.html", "_blank")
         } else {
             window.open("pagina-nav.html", "_blank")
         }
