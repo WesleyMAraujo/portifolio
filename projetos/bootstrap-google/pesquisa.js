7e39548a681d42f286b5f50a7732bbd9bb818702
Wesley Monteiro de Araujo <wesleymonteirodearaujo@gmail.com>
2023-04-10 07:39:43 -0300

    att

--- projetos/bootstrap-google/pesquisa.js
+++ projetos/bootstrap-google/pesquisa.js
@@ -1,9 +1,19 @@
-function pesquisar(){
+function pesquisar() {
     var pesquisou = document.getElementById('caixadepesquisa').value
     var wesley = 'wesley'
     if (pesquisou.toUpperCase() === wesley.toUpperCase()) {
-        window.open("wesley.html","_self")
+        window.open("wesley.html", "_self")
     } else {
-        window.open("pagina-nav.html","_blank")
+        window.open("pagina-nav.html", "_blank")
+    }
+}
+
+function pesquisar_barra(botao) {
+    if (botao.key === 'enter') {
+        if (pesquisou.toUpperCase() === wesley.toUpperCase()) {
+            window.open("wesley.html", "_self")
+        } else {
+            window.open("pagina-nav.html", "_blank")
+        }
     }
 }
